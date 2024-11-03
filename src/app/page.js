2c11b620c1a96815187f9037914fffde88fed34a
Yaxche <101154721+yaxchemanrique@users.noreply.github.com>
2024-11-02 19:26:13 -0700

    Update footer in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function Home() {
   return (
-    <main>
-      <h1>Hello Next!</h1>
-    </main>
+    <>
+      <main>
+        <h1>Hello Next!</h1>
+      </main>
+      <footer>
+        test by Yaxche
+      </footer>
+  </>
   );
 }
 
